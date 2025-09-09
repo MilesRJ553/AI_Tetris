1eb64ae7941289651803095d13bd68628aa359c4
MilesRJ553 <milesrobbjones01@gmail.com>
2025-09-09 19:27:54 +0100

    /changes to UI_READING for looking for other colours

--- AI_Tetris/UI_READING.cs
+++ AI_Tetris/UI_READING.cs
@@ -1,6 +1,8 @@
 using System.Drawing;
 using System;
 using System.Runtime.InteropServices;
+using WindowsInput;
+using WindowsInput.Native;
 
 class UI_READING
 {
@@ -11,8 +13,9 @@ class UI_READING
     const int SM_CXSCREEN = 0;
     const int SM_CYSCREEN = 1;
 
-    
-    public Color borderColour = Color.FromArgb(255, 0x24, 0x23, 0x23); // Colour of the border to ignore
+
+    //  public Color borderColour = Color.FromArgb(255, 0x24, 0x23, 0x23); // Colour of the border to ignore
+    public Color borderColour = Color.FromArgb(255, 0, 0, 0); // Colour of the border to ignore
     private Point sourceTopLeft = new Point(9999, 9999);  // The top left co'ordinate of the game screen
     private Point sourceBottomRight = new Point(0, 0); // The bottom right co'ordinate of the game screen
     private Size gameSize = new Size(0, 0);         // The size of the game screen (Width, Height in pixels)
@@ -82,6 +85,8 @@ class UI_READING
 
     public void getGameDimensions(Bitmap bmp, Color targetColor)
     {
+        IList<Point> points = new List<Point>();
+
         for (int y = 0; y < bmp.Height; y++)
         {
             for (int x = 0; x < bmp.Width; x++)
@@ -89,6 +94,7 @@ class UI_READING
                 Color pixelColor = bmp.GetPixel(x, y);
                 if (pixelColor == targetColor)
                 {
+                    points.Add(new Point(x, y));
                     if (this.sourceTopLeft.X + this.sourceTopLeft.Y > x + y)
                     {
                         this.sourceTopLeft = new Point(x, y);
@@ -103,9 +109,37 @@ class UI_READING
         int width = this.sourceBottomRight.X - this.sourceTopLeft.X;
         int height = this.sourceBottomRight.Y - this.sourceTopLeft.Y;
         this.gameSize = new Size(width, height);
+
+        this.paintPoints(points);
+
+    }
+
+    // For debugging only
+    private async void paintPoints(IList<Point> points)
+    {
+        InputSimulator inputSim = new InputSimulator();
+
+        await Task.Delay(1000); // Wait for text entry to complete
+/*
+        inputSim.Keyboard.TextEntry("mspaint");
+
+        Thread.Sleep(500);
+
+        inputSim.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+
+        Thread.Sleep(5000);
+*/
+        foreach (Point point in points)
+        {
+            Thread.Sleep(10);
+            inputSim.Mouse.MoveMouseTo(point.X * (65535.0 / GetSystemMetrics(SM_CXSCREEN)), point.Y * (65535.0 / GetSystemMetrics(SM_CYSCREEN)));
+            inputSim.Mouse.LeftButtonClick();
+        }
+
+
     }
     public static void Main()
-    {   
+    {
         Console.WriteLine("Start of program");
         Thread.Sleep(10000);
         UI_READING ui = new UI_READING();
